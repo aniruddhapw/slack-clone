c49d7f1946a3724caab7b402e35b6717a4c325ca
Aniruddha Prabhakar Wankhade <aniruddha@Aniruddhas-MacBook-Pro.local>
2025-03-19 16:13:44 +0530

    online offline

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  //baseURL: "http://localhost:9000",
+  baseURL: "http://localhost:9000",
   // baseURL: "https://slack-backend-uxfn.onrender.com",
-  baseURL: "https://slack-backend-direct.onrender.com",
+  // baseURL: "https://slack-backend-direct.onrender.com",
 });
 export default instance;
